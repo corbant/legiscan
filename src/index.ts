4f2aa29f822afca901c4be165e90b6b88cb2d961
Corey <coreyarice@gmail.com>
2025-09-12 19:02:47 -0300

    feat: export types and schemas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
 export { LegiScanClient } from './client';
 export * from './schemas/constants';
 export { indexedObjectToArray } from './utils';
+
+export type * from './schemas/amendment';
+export type * from './schemas/bill';
+export type * from './schemas/constants';
+export type * from './schemas/master-list';
+export type * from './schemas/monitor';
+export type * from './schemas/person';
+export type * from './schemas/roll-call';
+export type * from './schemas/search-result';
+export type * from './schemas/session';
+export type * from './schemas/supplement';
