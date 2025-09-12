053be48f693da3e1e3c40052882bf66bb6e9612f
Corey <coreyarice@gmail.com>
2025-09-12 12:39:16 -0300

    chore: format configuration files

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,44 +1,44 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
-    baseDirectory: import.meta.dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: import.meta.dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default defineConfig([
-{
-    ignores: ["dist/**", "dist/**/*.d.ts"],
-},
-{
-    
+  {
+    ignores: ['dist/**', 'dist/**/*.d.ts'],
+  },
+  {
     languageOptions: {
-        globals: {
-            ...globals.commonjs,
-            ...globals.node,
-        },
+      globals: {
+        ...globals.commonjs,
+        ...globals.node,
+      },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
 
-        parserOptions: {
-            project: "./tsconfig.eslint.json",
-        },
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+      },
     },
 
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint,
     },
 
     extends: compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended",
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
     ),
-}]);
+  },
+]);
